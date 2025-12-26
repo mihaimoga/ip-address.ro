2dbe9b76fb7d794944664e2efa881230feeaef99
Stefan-Mihai Moga <stefan-mihai.moga@nxp.com>
2025-12-26 19:31:54 +0200

    Updated href from apple-touch-icon
    Signed-off-by: Stefan-Mihai Moga <stefan-mihai.moga@nxp.com>

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 		<meta charset="UTF-8">
 		<title>IP Address</title>
 		<link rel="icon" type="image/png" href="romania-flag-square-icon-256.png">
-		<link rel="apple-touch-icon" href="https://www.moga.doctor/romania-flag-square-icon-256.png">
+		<link rel="apple-touch-icon" href="romania-flag-square-icon-256.png">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
 		<style type="text/css">
